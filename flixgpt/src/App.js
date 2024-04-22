d83cc6a85e78e7516cbb32da132073f7efb8e9b0
Meghana Bodapati ThirumalaNaidu <meghanabt@csu.fullerton.edu>
2024-04-22 14:58:16 -0700

    commiting initial changes

---
+++ flixgpt/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-3xl text-green-800 font-bold">
+      GPT Flix
+    </div>
+  );
+}
+
+export default App;
